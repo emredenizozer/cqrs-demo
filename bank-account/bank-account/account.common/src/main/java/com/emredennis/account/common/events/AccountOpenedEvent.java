1cda2541c982f2482c2322101598295f913488de
Emre Deniz Özer <emredenizozer@gmail.com>
2023-12-12 16:55:25 +0100

    Events were added

---
+++ bank-account/bank-account/account.common/src/main/java/com/emredennis/account/common/events/AccountOpenedEvent.java
@@ -0,0 +1,21 @@
+package com.emredennis.account.common.events;
+
+import com.emredennis.account.common.dto.AccountType;
+import com.emredennis.cqrs.core.events.BaseEvent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class AccountOpenedEvent extends BaseEvent {
+    private String accountHolder;
+    private AccountType accountType;
+    private Date createdDate;
+    private double openingBalance;
+}
