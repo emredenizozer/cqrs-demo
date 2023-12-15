b31eeba57f645a8feed1781c2937594e94b30ce7
Emre Deniz Özer <emredenizozer@gmail.com>
2023-12-15 13:50:48 +0100

    EventConsumer related code was added for getting messages from Kafka

---
+++ bank-account/bank-account/account.query/src/main/java/com/emredennis/account/query/infrastructure/consumers/EventConsumer.java
@@ -0,0 +1,15 @@
+package com.emredennis.account.query.infrastructure.consumers;
+
+import com.emredennis.account.common.events.AccountClosedEvent;
+import com.emredennis.account.common.events.AccountOpenedEvent;
+import com.emredennis.account.common.events.FundsDepositedEvent;
+import com.emredennis.account.common.events.FundsWithdrawnEvent;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
+
+public interface EventConsumer {
+    void consume(@Payload AccountOpenedEvent event, Acknowledgment ack);
+    void consume(@Payload FundsDepositedEvent event, Acknowledgment ack);
+    void consume(@Payload FundsWithdrawnEvent event, Acknowledgment ack);
+    void consume(@Payload AccountClosedEvent event, Acknowledgment ack);
+}
