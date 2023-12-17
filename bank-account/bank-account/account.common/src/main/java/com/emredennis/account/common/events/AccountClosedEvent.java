4292e1568d87722b274842a59060903ec725efba
Emre Deniz Özer <emredenizozer@gmail.com>
2023-12-17 22:40:08 +0100

    NoArgsConstructor was added for Kafka consumer

--- bank-account/bank-account/account.common/src/main/java/com/emredennis/account/common/events/AccountClosedEvent.java
+++ bank-account/bank-account/account.common/src/main/java/com/emredennis/account/common/events/AccountClosedEvent.java
@@ -2,9 +2,11 @@ package com.emredennis.account.common.events;
 
 import com.emredennis.cqrs.core.events.BaseEvent;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 public class AccountClosedEvent extends BaseEvent {
 }
