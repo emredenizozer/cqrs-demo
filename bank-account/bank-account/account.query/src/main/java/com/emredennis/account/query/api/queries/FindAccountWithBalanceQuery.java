f9da4514b8e606441ffafd7d6f3a01df5e9cb2db
Emre Deniz Özer <emredenizozer@gmail.com>
2023-12-18 11:59:06 +0100

    Missing lombok annotations were added

--- bank-account/bank-account/account.query/src/main/java/com/emredennis/account/query/api/queries/FindAccountWithBalanceQuery.java
+++ bank-account/bank-account/account.query/src/main/java/com/emredennis/account/query/api/queries/FindAccountWithBalanceQuery.java
@@ -2,7 +2,11 @@ package com.emredennis.account.query.api.queries;
 
 import com.emredennis.account.query.api.dto.EqualityType;
 import com.emredennis.cqrs.core.queries.BaseQuery;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class FindAccountWithBalanceQuery extends BaseQuery {
     private EqualityType equalityType;
     private double balance;
