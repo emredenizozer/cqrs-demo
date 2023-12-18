50b6f8d3a8a218137a12f208d8c2a3a1cfd39db8
Emre Deniz Özer <emredenizozer@gmail.com>
2023-12-18 22:15:47 +0100

    Remove extra space

--- cqrs-es/cqrs.core/src/main/java/com/emredennis/cqrs/core/commands/CommandHandlerMethod.java
+++ cqrs-es/cqrs.core/src/main/java/com/emredennis/cqrs/core/commands/CommandHandlerMethod.java
@@ -1,6 +1,6 @@
 package com.emredennis.cqrs.core.commands;
 
 @FunctionalInterface
-public interface CommandHandlerMethod<T extends  BaseCommand> {
+public interface CommandHandlerMethod<T extends BaseCommand> {
     void handle(T command);
 }
